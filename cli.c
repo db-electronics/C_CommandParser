206145e9270d33acc68706aa4265cce87751a901
René Richard <rene@db-electronics.ca>
2019-03-05 16:01:15 -0500

    Added file header to cli.c

--- cli.c
+++ cli.c
@@ -1,4 +1,24 @@
-
+/*******************************************************************//**
+ *  \file   cli.c
+ *  \author René Richard
+ *  \brief  This program tokenizes commands and associates them to
+ *          function calls 
+ *
+ *  \copyright This file is part of Command Parser.
+ *
+ *   Command Parser is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Command Parser is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Command Parser.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <stdio.h>
 #include <string.h>
